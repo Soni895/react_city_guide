a3883ad287354596cab6714581cc870de8b6a157
soni895 <darshansoni895@gmail.com>
2023-06-18 17:56:21 +0530

    initial commi

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import Cards from './Cards'
+const  App = () => {
+  return (
+    <div>
+      <h1>Plan with Darshan</h1>
+       <Cards></Cards>
+    </div>
+
+  );
+};
+
+export default App;
---
+++ src/Cards.js
@@ -0,0 +1,12 @@
+import "./Cards.css";
+
+function Cards()
+{
+    return(
+
+        <div>
+            hi darshan
+        </div>
+    );
+};
+export default Cards;
